7885f288f70e57db4b156edfeeca10969b6e3ec7
NOMAD <gkhn_tsn_9@hotmail.com>
2021-10-30 00:55:05 +0300

    Add files via upload

---
+++ Destek/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace Destek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            topla topla = new topla();
+            int[] veri1 = new int[] { 1, 2, 3, 4, 5, 6};
+            int[] veri2 = new int[] { 7, 8, 9, 10, 11, 12};
+
+            Console.WriteLine("veri1: " + topla.toplam(veri1));
+            Console.WriteLine("veri2: " + topla.toplam(veri2));
+            Console.Read();
+        }
+    }
+}
---
+++ Destek/topla.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destek
+{
+    internal class topla
+    {
+        public int toplam(int[] veriler)
+        {
+            int plus = 0;
+            for (int i = 0; i < veriler.Length; i++)
+            {
+                plus += veriler[i];
+            }
+            return plus;
+        }
+    }
+}
