56579fdfef82f17779d905172f5ae08c3387e49c
NOMAD <gkhn_tsn_9@hotmail.com>
2021-10-30 01:21:21 +0300

    Güncelleme

--- Destek/Program.cs
+++ Destek/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using Hesaplama;
 using System;
 
 namespace Destek
@@ -7,12 +8,13 @@ namespace Destek
     {
         static void Main(string[] args)
         {
-            topla topla = new topla();
+            BasitHesaplama basit = new BasitHesaplama();
+
             int[] veri1 = new int[] { 1, 2, 3, 4, 5, 6};
             int[] veri2 = new int[] { 7, 8, 9, 10, 11, 12};
 
-            Console.WriteLine("veri1: " + topla.toplam(veri1));
-            Console.WriteLine("veri2: " + topla.toplam(veri2));
+            Console.WriteLine("veri1: " + basit.toplam(veri1));
+            Console.WriteLine("veri2: " + basit.toplam(veri2));
             Console.Read();
         }
     }
---
+++ Hesaplama/BasitHesaplama.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hesaplama
+{
+    public class BasitHesaplama
+    {
+        public int toplam(int[] veriler)
+        {
+            int plus = 0;
+            for (int i = 0; i < veriler.Length; i++)
+            {
+                plus += veriler[i];
+            }
+            return plus;
+        }
+    }
+}
