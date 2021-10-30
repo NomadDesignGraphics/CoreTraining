de27f92a57f195d65339d66b04ff5463d6abc527
NOMAD <gkhn_tsn_9@hotmail.com>
2021-10-31 00:03:12 +0300

    web-testing

---
+++ CoreWEB/Controllers/Control.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWEB.Controllers
+{
+    [Route("control")]
+    public class Control : ControllerBase
+    {
+        [Route("kontrol")]
+        public string kontrol()
+        {
+            return "işe yarıyor evlat";
+        }
+
+        [Route("anasayfa")]
+        public string ANASAYFA()
+        {
+            return "ANA SAYFA işe yarıyor evlat";
+        }
+    }
+}
